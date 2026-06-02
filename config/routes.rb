e0c516720f1993567daf0196b33ef724edcc3909
Claire Delacroix <clairedelacroix71@gmail.com>
2026-06-02 15:48:09 +0800

    install solid gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :projects do
+   resources :cards, only: [:show, :update, :edit, :create]
+  end
+
+  resources :projects do
+   resources :chat_threads
+  end
+
+  resources :chat_threads do
+   resources :chat_messages
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
