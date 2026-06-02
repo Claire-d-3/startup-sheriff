77c9c507b6922c17c87f694a60882b2f6b127f3e
Claire Delacroix <clairedelacroix71@gmail.com>
2026-06-02 16:09:11 +0800

    added first name to user

--- db/migrate/20260602080552_add_first_name_to_users.rb
+++ db/migrate/20260602080552_add_first_name_to_users.rb
@@ -1,5 +1,5 @@
 class AddFirstNameToUsers < ActiveRecord::Migration[8.1]
   def change
-    add_column :users, :first_name, :string, null:false
+    add_column :users, :first_name, :text, null: false
   end
 end
